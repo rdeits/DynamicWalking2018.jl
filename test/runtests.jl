4673e5f1be8b8b5ff0f7f5e17fbb0b127ecf6720
Robin Deits <robin.deits@gmail.com>
2018-05-17 14:30:48 -0400

    more testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,10 @@ const module_tempdir = joinpath(Base.tempdir(), string(module_name(DynamicWalkin
     end
     jupyter = IJulia.jupyter
     for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
+        if contains(f, "Julia is Fast") && !is_linux()
+            # Can't run the gcc demo on osx or windows
+            continue 
+        end
         notebook = joinpath("..", "notebooks", f)
         output = joinpath(outputdir, f)
         @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=500`); true end
