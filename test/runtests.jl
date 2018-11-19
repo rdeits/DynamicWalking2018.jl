443de05898e32d1d71b711fb0eb318c1b1be33ef
Robin Deits <robin.deits@gmail.com>
2018-11-18 19:16:09 -0500

    test fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,26 @@
 using DynamicWalking2018
-using Base.Test
-
-const module_tempdir = joinpath(Base.tempdir(), string(module_name(DynamicWalking2018)))
+using Test
 
 using Conda
-if is_windows()
+if Sys.iswindows()
     Conda.add("nbconvert") # jupyter doesn't seem to come with nbconvert on windows?
 end
 
 @testset "example notebooks" begin
     using IJulia
 
-    outputdir = module_tempdir
-    if !isdir(outputdir)
-        mkpath(outputdir)
-    end
-    jupyter = IJulia.jupyter
-    for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
-        if contains(f, "Julia is Fast") && !is_linux()
-            # Can't run the gcc demo on osx or windows
-            continue
-        end
-        notebook = joinpath("..", "notebooks", f)
-        output = joinpath(outputdir, f)
-        cd dirname(notebook) do
-            @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=500`); true end
+    mktempdir() do outputdir
+        jupyter = IJulia.jupyter
+        for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
+            if occursin("Julia is Fast", f) && !is_linux()
+                # Can't run the gcc demo on osx or windows
+                continue
+            end
+            notebook = joinpath("..", "notebooks", f)
+            output = joinpath(outputdir, f)
+            cd(dirname(notebook)) do
+                @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=500`); true end
+            end
         end
     end
 end
