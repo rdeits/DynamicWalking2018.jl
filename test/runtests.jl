4853d9e0d3eeaa858776d1c0529fd05a1085c0e0
Robin Deits <robin.deits@gmail.com>
2018-11-19 19:32:22 -0500

    Try using nbinclude instead

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,23 @@
 using DynamicWalking2018
 using Test
+using NBInclude
 
 using Conda
-if Sys.iswindows()
-    Conda.add("nbconvert") # jupyter doesn't seem to come with nbconvert on windows?
-end
 Conda.add("numpy")
 
 @testset "example notebooks" begin
-    using IJulia
-
     mktempdir() do outputdir
-        jupyter = IJulia.jupyter
+        # jupyter = IJulia.jupyter
         for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
             if occursin("Julia is Fast", f) && !Sys.islinux()
                 # Can't run the gcc demo on osx or windows
                 continue
             end
+            notebook_module = Module()
+            @eval(notebook_module, using InteractiveUtils)
             notebook = joinpath("..", "notebooks", f)
-            output = joinpath(outputdir, f)
             cd(dirname(notebook)) do
-                @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=500`); true end
+                nbinclude(notebook_module, notebook)
             end
         end
     end
